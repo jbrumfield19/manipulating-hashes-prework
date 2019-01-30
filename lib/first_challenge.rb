7dd36244102da17dfd4b96fcf821b8fc4fa5245f
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-30 16:32:45 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,7 @@ def first_challenge
 
   #your code here
 
-contacts['Jon Snow'][:favorite_icecream_flavors]<< "mint chip"
+contacts['Freddy Mercury'].delete_if?("strawberry")
 puts contacts
   #remember to return your newly altered contacts hash!
   contacts
