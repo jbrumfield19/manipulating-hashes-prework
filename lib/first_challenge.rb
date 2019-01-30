b8168e4a05b7c0229627c55ac8ec0ff08e9684b8
Jessica Brumfield <jbrumfield1999@gmail.com>
2019-01-30 16:53:56 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -15,7 +15,8 @@ def first_challenge
 
   #your code here
 
-
+contacts['Freddy Mercury'][:favorite_icecream_flavors].shift
+puts contacts
   #remember to return your newly altered contacts hash!
   contacts
 end
